185947a8e33c320c3ded14216dbf9262d2997a41
jason-allison <jasona@tacc.utexas.edu>
2021-12-30 21:21:48 -0600

    initial commit

---
+++ RTSLabsTest.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import RTSLabs
+#test cases      
+# myObj = RTSLabs()
+# myList = [1,8,2,8,4,76,3,2,9]
+# myInt = 4
+# print(myObj.aboveBelow(myList,myInt))
+# print(myObj.stringRotation("thisIsAnewString",4))
+
+# myObj = RTSLabs()
+# myList = [1,"o",2,8,4,76,3,2,9]
+# myInt = 4
+# print(myObj.aboveBelow(myList,myInt))
+# print(myObj.stringRotation("thisIsAnewString",4))
+
+# myObj = RTSLabs()
+# myList = [1,8,2,8,4,76,3,2,9]
+# myInt = ""
+# print(myObj.aboveBelow(myList,myInt))
+# print(myObj.stringRotation("thisIsAnewString",4))
+myList = [1,8,2,8,4,76,3,2,9]
+myInt = 6
+print(RTSLabs.aboveBelow(myList,myInt))
+print(RTSLabs.stringRotation(7,4))
+
+# myObj = RTSLabs()
+# myList = [1,8,2,8,4,76,3,2,9]
+# myInt = 6
+# print(myObj.aboveBelow(myList,myInt))
+# print(myObj.stringRotation("thi",4))
