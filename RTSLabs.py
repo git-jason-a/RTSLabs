0674db6d44bb0e32d4906bb107486154796f92e6
jason-allison <jasona@tacc.utexas.edu>
2022-01-02 14:48:51 -0600

    finalize RTSLabs.py

--- RTSLabs.py
+++ RTSLabs.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
+
+
 class RTSLabs:
     
-    aboveBelowDict = {"above" : 0, "below" : 0}
-    outputString = ""
-    
+    def __init__(self):
+      self.aboveBelowDict = {"above" : 0, "below" : 0}
+      self.outputString = ""
+
     def aboveBelow(self,aboveBelowList,aboveBelowInt):     
         assert(type(aboveBelowInt) is int),("aboveBelowInt " + aboveBelowInt + " is not an int.")
         aboveBelowDict = self.aboveBelowDict   
@@ -27,3 +30,40 @@ class RTSLabs:
 
 
 
+#test cases (uncomment to run)
+
+#1. success
+
+#myObj = RTSLabs() 
+#myList = [1,8,2,8,4,76,3,2,9]
+#myInt = 4
+#print(myObj.aboveBelow(myList,myInt))
+#print(myObj.stringRotation("thisIsAnewString",4))
+
+#2. success but will inform that o is not an int
+#myObj1 = RTSLabs()
+#myList = [1,"o",2,8,4,76,3,2,9]
+#myInt = 4
+#print(myObj1.aboveBelow(myList,myInt))
+#print(myObj1.stringRotation("thisIsAnewString",4))
+
+#3. fail myInt value is not type int
+#myObj2 = RTSLabs()
+#myList = [1,8,2,8,4,76,3,2,9]
+#myInt = ""
+#print(myObj2.aboveBelow(myList,myInt))
+#print(myObj2.stringRotation("thisIsAnewString",4))
+
+#4. fail stringRotation 1st argument is not a string
+#myObj3 = RTSLabs()
+#myList = [1,8,2,8,4,76,3,2,9]
+#myInt = 6
+#print(myObj3.aboveBelowDict)
+#print(myObj3.stringRotation(7,4))
+
+#5. fail stringRotation 2nd argument is longer that string length, unable to perform rotation
+myObj4 = RTSLabs()
+myList = [1,8,2,8,4,76,3,2,9]
+myInt = 6
+print(myObj4.aboveBelow(myList,myInt))
+print(myObj4.stringRotation("thi",4))
---
+++ __init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Jason Allison'
+__email__ = 'jason.robert.allison@gmail.com'
+__version__ = '0.1.0'
